3a086f7fc411dfc8542a5619254bb11e34832206
raghadromaneh <raghadromani22@gmail.com>
2022-09-07 11:47:54 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  playNote(int noteNumber) {
-    AudioCache().play("note$noteNumber.wav");
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // drawer: Drawer(),
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(70),
           child: AppBar(
@@ -35,122 +31,75 @@ class MyApp extends StatelessWidget {
             ]),
           ),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              height: 93,
-              child: RaisedButton(
-                splashColor: Color.fromARGB(255, 122, 235, 124),
-                child: Text(
-                  "D",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 56, 174, 62)),
-                ),
-                color: Color.fromARGB(255, 46, 253, 53),
-                onPressed: () {
-                  playNote(1);
-                },
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Bar(
+                  barColor: Color.fromARGB(255, 46, 253, 53),
+                  barName: "D",
+                  noteNumber: 1),
+              Bar(
+                barColor: Color.fromARGB(255, 253, 227, 0),
+                barName: "E",
+                noteNumber: 2,
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 93,
-              child: RaisedButton(
-                splashColor: Color.fromARGB(255, 255, 238, 89),
-                child: Text(
-                  "E",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 202, 149, 4)),
-                ),
-                color: Color.fromARGB(255, 255, 234, 0),
-                onPressed: () {
-                  playNote(2);
-                },
+              Bar(
+                barColor: Color.fromARGB(255, 255, 128, 0),
+                barName: "F",
+                noteNumber: 3,
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 93,
-              child: RaisedButton(
-                splashColor: Colors.orange,
-                child: Text(
-                  "F",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 239, 83, 27)),
-                ),
-                color: Color.fromARGB(255, 255, 128, 0),
-                onPressed: () {
-                  playNote(3);
-                },
+              Bar(
+                barColor: Color.fromARGB(255, 245, 16, 0),
+                barName: "G",
+                noteNumber: 4,
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 93,
-              child: RaisedButton(
-                splashColor: Colors.red,
-                child: Text(
-                  "G",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 172, 3, 3)),
-                ),
-                color: Color.fromARGB(255, 245, 16, 0),
-                onPressed: () {
-                  playNote(4);
-                },
+              Bar(
+                barColor: Color.fromARGB(255, 232, 42, 102),
+                barName: "A",
+                noteNumber: 5,
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 93,
-              child: RaisedButton(
-                splashColor: Color.fromARGB(255, 227, 131, 163),
-                child: Text(
-                  "A",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 150, 5, 73)),
-                ),
-                color: Color.fromARGB(255, 232, 42, 102),
-                onPressed: () {
-                  playNote(5);
-                },
+              Bar(
+                barColor: Colors.white,
+                barName: "B",
+                noteNumber: 6,
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 91,
-              child: RaisedButton(
-                splashColor: Colors.purple,
-                child: Text(
-                  "B",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 201, 206, 210)),
-                ),
-                color: Colors.white,
-                onPressed: () {
-                  playNote(6);
-                },
+              Bar(
+                barColor: Color.fromARGB(255, 2, 6, 242),
+                barName: "C",
+                noteNumber: 7,
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 91,
-              child: RaisedButton(
-                splashColor: Colors.blueAccent,
-                child: Text(
-                  "C",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 0, 30, 55)),
-                ),
-                color: Color.fromARGB(255, 2, 6, 242),
-                onPressed: () {
-                  playNote(7);
-                },
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class Bar extends StatelessWidget {
+  Bar({this.barColor, this.barName, this.noteNumber});
+
+  playNote(int noteNumber) {
+    AudioCache().play("note$noteNumber.wav");
+  }
+
+  Color? barColor;
+  String? barName;
+  int? noteNumber;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 93,
+      child: MaterialButton(
+        child: Text(
+          barName.toString(),
+          style: TextStyle(fontSize: 30, color: Colors.grey),
+        ),
+        color: barColor,
+        onPressed: () {
+          playNote(noteNumber!);
+        },
       ),
     );
   }
